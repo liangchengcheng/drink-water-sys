fd88326f4dcc6b8d345cbc0dc38f0cf3f8ee90ff
liangchengcheng <1038127753@qq.com>
2017-11-13 08:27:57 +0800

    add exception class file
    add exception class file

---
+++ drink-water-api/src/main/java/com/lcc/drinkwater/excel/exceptions/IllegalGroupIndexException.java
@@ -0,0 +1,10 @@
+package com.lcc.drinkwater.excel.exceptions;
+
+public class IllegalGroupIndexException extends Exception {
+	
+	private static final long serialVersionUID = 7725478743860387475L;
+
+	public IllegalGroupIndexException(String message) {
+        super(message);
+    }
+}
---
+++ drink-water-api/src/main/java/com/lcc/drinkwater/excel/exceptions/TimeMatchFormatException.java
@@ -0,0 +1,12 @@
+package com.lcc.drinkwater.excel.exceptions;
+
+
+public class TimeMatchFormatException extends Exception {
+	
+	private static final long serialVersionUID = 206910143412957809L;
+
+	public TimeMatchFormatException(String message) {
+        super(message);
+    }
+
+}
